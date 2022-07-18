e807dc08fc95d551e39d550ef55467cbc0c56a6e
taher-ahmed <taher.ahmed2004@hotmail.com>
2022-07-18 20:24:04 +0100

    change 6

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Type racer</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+  <header>
+    <div class="header">
+      <div class="logo_container">
+        <h1> SIUUU</h1>     
+      </div>
+    </div>
+  </header>
+</body>
+
+</html>
